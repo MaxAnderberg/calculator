8ba30abe80ac2f5a370e1220cc7b29681cfdaafc
Max Anderberg <maxanderberg@gmail.com>
2021-02-09 15:35:37 +0100

    Added the rest of the operations to work as well

--- scripts/app.js
+++ scripts/app.js
@@ -50,9 +50,23 @@ function factorial(num) {
 }
 
 function operator(op,x,y){
+    console.log("This is the operator function")
+    console.log("this is x: " + x + " this is y: " + y)
+    console.log("this is the operator: " + op)
     if(op == "add"){
         console.log(add(parseInt(x),parseInt(y)))
         return add(x,y);
+    } else if(op == "subtract"){
+        console.log(subtract(parseInt(x),parseInt(y)))
+        return subtract(x,y);
+    } else if(op == "divide"){
+        console.log(op == "DIVIDE ME DADDY")
+        console.log(divide(parseInt(x),parseInt(y)))
+        // return divide(x,y);
+    } else if(op == "multiply"){
+        console.log("MULTIPLY ME DADDY")
+        console.log(multiply(parseInt(x),parseInt(y)))
+        return multiply(x,y);
     }
 }
 
@@ -64,13 +78,14 @@ function setOperator(operator){
 
 function setXvalue(){
     tmpValueX = tmpValue;
-    tmpValue = ""
+    console.log("This is SetXvalue, valueX: " + tmpValueX + " and this is tmpValue: " + tmpValue)
+    resetNumber();
 }
 
 function setValue(value){
     tmpValue += value;
     displayValue();
-    console.log(tmpValue)
+    console.log("setValue: " + tmpValue)
 }
 
 function displayValue(){
@@ -78,15 +93,24 @@ function displayValue(){
 }
 
 function getNumber(){
+    console.log("getNumber: " + tmpValue)
     return tmpValue;
 }
 
 function equals(){
-    console.log("equal here " + tmpOpertor + " " + tmpValueX, + " " + tmpValue)
+    console.log("equal here " + tmpOpertor + " " + tmpValueX + " " + tmpValue)
 
     operator(tmpOpertor,tmpValueX,tmpValue)
 }
 
 function resetNumber(){
-    return tmpValue = "";
+    tmpValue = "";
+    console.log("resetNumber: " + tmpValue)
+}
+
+function clear(){
+    tmpValue = "";  
+    tmpValueX = "";
+    tmpValueY = "";
+    tmpOpertor = "";
 }
